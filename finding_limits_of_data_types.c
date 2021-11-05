870283131e8a58fd617dc94305ef4bff5ee635ee
JonConstantine01 <93780397+JonConstantine01@users.noreply.github.com>
2021-11-05 11:09:42 +0530

    lets go c stuff

---
+++ finding_limits_of_data_types.c
@@ -0,0 +1,9 @@
+//program to find limits of data types
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+int main(void)
+{
+    printf("sizeof(char) = %u\n", sizeof(char));
+    printf("sizeof(short) = %u\n", sizeof(short));
+}
