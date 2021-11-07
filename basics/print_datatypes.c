2dcde9f8c904cb86538d23cf2c73b82a64ba74c0
JonConstantine01 <93780397+JonConstantine01@users.noreply.github.com>
2021-11-07 07:21:03 +0530

    datatype char

--- basics/print_datatypes.c
+++ basics/print_datatypes.c
@@ -2,12 +2,19 @@
 int main(void){
     int age;
     float eage;
+    char gender;
+    /*Int datatype print*/
     printf("Enter your age : ");
     scanf("%d",&age);
     printf("Your age is : %d",age);
-
+    /*float datatype print*/
     printf("\nEnter exactly how old are you : ");
     scanf("%f", &eage);
     printf("You are precisely %0.2f years old", eage);
+    /*char datatype print*/
+    /*have put space before %c*/
+    printf("\nEnter your gender : ");
+    scanf(" %c", &gender);
+    printf("Your gender is : %c", gender);
     return 0;
 }
