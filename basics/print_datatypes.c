6ebaab6f2fa6747aad6086cb9b8ea9cf294812bd
JonConstantine01 <93780397+JonConstantine01@users.noreply.github.com>
2021-11-07 07:21:03 +0530

    datatype float

--- basics/print_datatypes.c
+++ basics/print_datatypes.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 int main(void){
     int age;
+    float eage;
     printf("Enter your age : ");
     scanf("%d",&age);
     printf("Your age is : %d",age);
+
+    printf("\nEnter exactly how old are you : ");
+    scanf("%f", &eage);
+    printf("You are precisely %0.2f years old", eage);
     return 0;
 }
