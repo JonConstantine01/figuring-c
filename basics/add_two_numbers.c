8216fd3f7d1e34d15f68b1b451d8beeeb08bae03
JonConstantine01 <93780397+JonConstantine01@users.noreply.github.com>
2025-08-02 19:29:15 +0530

    file structure change

--- basics/add_two_numbers.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void) {
-    float a, b, sum;
-    printf("Enter two numbers : \n");
-    scanf("%f%f", &a, &b);
-    sum = a+b;
-    printf("sum of the entered two numbers is : %f", sum);
-}
--- basics/print_with_delimiters.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() {
-    int basic;
-    float hra;
-    /*We use the delimiter the : colon so the input can also be entered as -> 45:74.56 and output will be basic=45, hra=74.56*/
-    scanf("%d:%f",&basic,&hra);
-    printf("basic=%d, hra=%f", basic, hra);
-}
